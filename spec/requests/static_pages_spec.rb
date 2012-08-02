6de8bcbe50283b917d438051900df945259922fc
asansom <asansom@willstream.com>
2012-08-02 11:33:20 +0100

    Updated .gitignore

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,9 +4,17 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the content ''" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
     end
   end
+
+  describe "About Page" do
+    
+     it "should have content 'Contact Us'" do
+      visit '/static_pages/about'
+      page.should have_content('Contact')
+  end
+
 end
